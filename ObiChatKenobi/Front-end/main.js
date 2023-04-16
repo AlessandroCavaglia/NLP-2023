73990e4c9d433f5d0736a5c7fa7594c6542da7b2
AlbertoCampini <campinialberto@gmail.com>
2023-04-16 14:35:30 +0200

    Aggiunta text to speech per fare audio al bot

--- ObiChatKenobi/Front-end/main.js
+++ ObiChatKenobi/Front-end/main.js
@@ -10,6 +10,24 @@ let icon_send = document.getElementById('icon_send')
 let icon_audio = document.getElementById('icon_audio')
 let status = document.getElementById('status')
 
+//Speech recognition
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+
+const recognition = new SpeechRecognition();
+const speechRecognitionList = new SpeechGrammarList();
+
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+//recognition.lang = "en-US";
+recognition.lang = "IT-IT";
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+
+
 input_message.addEventListener('input', ()=>{
     if(input_message.value !== ""){
         icon_send.classList.remove('d-none')
@@ -24,13 +42,28 @@ input_message.addEventListener('input', ()=>{
 })
 
 icon_audio.addEventListener('click',()=>{
-
+  recognition.start();
+  icon_audio.style.color = "red";
 })
 
-const send_message = function (){
+recognition.onresult = (event) => {
+  const response = event.results[0][0].transcript;
+  send_message(response)
+  icon_audio.style.color = '#999';
+};
+
+recognition.onerror = (event) => {
+  console.log(`Error occurred in recognition: ${event.error}`);
+};
+
+
+const send_message = function (value){
+    if(typeof value === 'object')
+        value = input_message.value;
     icon_audio.classList.remove('d-none')
     icon_send.classList.add('d-none')
-    chat.innerHTML = chat.innerHTML + '<div class="message parker">'+input_message.value+'<div class="timeMessage">'+getCurrentTime()+'</div></div>'
+    console.log(value)
+    chat.innerHTML = chat.innerHTML + '<div class="message parker">'+value+'<div class="timeMessage">'+getCurrentTime()+'</div></div>'
     input_message.value = ''
     setTimeout(()=>{manageMessage(1)},500)
     chat.scrollTop = chat.scrollHeight - chat.clientHeight;
@@ -39,7 +72,7 @@ const send_message = function (){
 icon_send.addEventListener('click', send_message)
 input_message.addEventListener('keypress', (e)=>{
      if (e.key === 'Enter' && input_message.value !== "") {
-      send_message()
+      send_message(input_message.value)
     }
 })
 
