be0c1bb53709f94da5f9749be53d4753f304c512
Alessandro Cavaglià <alessandro.cavaglia@di.uito.it>
2023-06-04 10:53:17 +0200

    Esercizio 3, provata riformattazione legit ma creati loop infiniti

--- Radicioni/es3.py
+++ Radicioni/es3.py
@@ -32,6 +32,38 @@ def learn_bi_gram_model(dataset):
 
     return biTwitter_probabilities
 
+def count_bigrams(tokens):
+    bigram_counts = {}
+    for i in range(len(tokens) - 1):
+        current_token = tokens[i]
+        next_token = tokens[i + 1]
+        if current_token not in bigram_counts:
+            bigram_counts[current_token] = {}
+        if next_token not in bigram_counts[current_token]:
+            bigram_counts[current_token][next_token] = 0
+        bigram_counts[current_token][next_token] += 1
+    return bigram_counts
+
+def learn_bi_gram_model_legit(dataset):
+    tokens = [nltk.word_tokenize(sentence) for sentence in [elem[0] for elem in dataset]]
+    tokens = [token for sublist in tokens for token in sublist]
+    bigram_counts = count_bigrams(tokens)
+
+    vocab_size = len(set(tokens))
+
+    # Calculating probabilities with Laplace smoothing
+    biTwitter_probabilities = {}
+    for first_token in bigram_counts:
+        sum=0
+        for second_token in bigram_counts[first_token]:
+            sum+=bigram_counts[first_token][second_token]
+
+        for second_token in bigram_counts[first_token]:
+            bi_gram=(first_token,second_token)
+            biTwitter_probabilities[bi_gram] = (bigram_counts[first_token][second_token] + 1) / (sum + vocab_size)
+
+    return biTwitter_probabilities
+
 def learn_tri_gram_model(dataset):
     tokens = [nltk.word_tokenize(sentence) for sentence in [elem[1] for elem in dataset]]
     tokens = [token for sublist in tokens for token in sublist]
@@ -48,18 +80,19 @@ def learn_tri_gram_model(dataset):
 
     return triTwitter_probabilities
 
-def generate_text(prob):
+def generate_text_bigram(prob):
     current_word = "["
     generated_text = [current_word]
-
     while current_word != "]":
+        if(len(generated_text)>50):
+            break
         if (len(generated_text) > 4):
             next_word = max(prob, key=lambda bi_gram: bi_gram[0] == current_word)[1]
             generated_text.append(next_word)
             current_word = next_word
         else:
             possible_next_words = [word for word in prob if word[0] == current_word and word[1] != "."]
-            probabilities = [prob[word] for word in possible_next_words]
+            probabilities = [prob[(word)] for word in possible_next_words]
             next_word = random.choices(possible_next_words, probabilities)[0][1]
             generated_text.append(next_word)
             current_word = next_word
@@ -92,12 +125,13 @@ if __name__ == "__main__":
     #for sent in df:
      #   print(sent)
 
-    bi_prob=learn_bi_gram_model(df)
+    bi_prob=learn_bi_gram_model_legit(df)
+
     tri_prob=learn_tri_gram_model(df)
     print("BIGRAM GENERATED SENTENCES")
-    print(generate_text(bi_prob))
-    print(generate_text(bi_prob))
-    print(generate_text(bi_prob))
+    print(generate_text_bigram(bi_prob))
+    print(generate_text_bigram(bi_prob))
+    print(generate_text_bigram(bi_prob))
     print("\nTRIGRAM GENERATED SENTENCES")
     print(generate_text_trigram(tri_prob))
     print(generate_text_trigram(tri_prob))
