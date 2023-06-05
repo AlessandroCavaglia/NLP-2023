d98a15b471ce5e0781b539244c8629f3e4d2aa63
REPLYNET\a.cavaglia <a.cavaglia@reply.it>
2023-06-05 09:34:44 +0200

    Small fixes

--- Radicioni/es3.py
+++ Radicioni/es3.py
@@ -16,22 +16,6 @@ def read_csv(filename):
                 dataset.append((sentence_bi,sentence_tri))
     return dataset
 
-def learn_bi_gram_model(dataset):
-    tokens = [nltk.word_tokenize(sentence) for sentence in [elem[0] for elem in dataset]]
-    tokens = [token for sublist in tokens for token in sublist]
-    tokens = list(bigrams(tokens))
-    distribution = FreqDist(tokens)
-
-    vocab_size = len(set(tokens))
-
-    # Calculating probabilities with Laplace smoothing
-    biTwitter_probabilities = {}
-    for bi_gram in distribution:
-        previous_word = bi_gram[0]
-        biTwitter_probabilities[bi_gram] = (distribution[bi_gram] + 1) / (distribution[previous_word] + vocab_size)
-
-    return biTwitter_probabilities
-
 def count_bigrams(tokens):
     bigram_counts = {}
     for i in range(len(tokens) - 1):
@@ -44,7 +28,7 @@ def count_bigrams(tokens):
         bigram_counts[current_token][next_token] += 1
     return bigram_counts
 
-def learn_bi_gram_model_legit(dataset):
+def learn_bi_gram_model(dataset):
     tokens = [nltk.word_tokenize(sentence) for sentence in [elem[0] for elem in dataset]]
     tokens = [token for sublist in tokens for token in sublist]
     bigram_counts = count_bigrams(tokens)
@@ -64,46 +48,56 @@ def learn_bi_gram_model_legit(dataset):
 
     return biTwitter_probabilities
 
+def count_trigrams(tokens):
+    trigram_counts = {}
+    for i in range(len(tokens) - 2):
+        current_token = tokens[i]
+        next_token = tokens[i + 1]
+        next_next_token = tokens[i + 2]
+        if (current_token, next_token) not in trigram_counts:
+            trigram_counts[(current_token, next_token)] = {}
+        if next_next_token not in trigram_counts[(current_token, next_token)]:
+            trigram_counts[(current_token, next_token)][next_next_token] = 0
+        trigram_counts[(current_token, next_token)][next_next_token] += 1
+    return trigram_counts
 def learn_tri_gram_model(dataset):
     tokens = [nltk.word_tokenize(sentence) for sentence in [elem[1] for elem in dataset]]
     tokens = [token for sublist in tokens for token in sublist]
-    tokens = list(trigrams(tokens))
-    distribution = FreqDist(tokens)
+    trigram_counts = count_trigrams(tokens)
 
     vocab_size = len(set(tokens))
 
     # Calculating probabilities with Laplace smoothing
     triTwitter_probabilities = {}
-    for tri_gram in distribution:
-        previous_word = tri_gram[:-1]
-        triTwitter_probabilities[tri_gram] = (distribution[tri_gram] + 1) / (distribution[previous_word] + vocab_size)
+    for first_bigram in trigram_counts:
+        sum = 0
+        for third_token in trigram_counts[first_bigram]:
+            sum += trigram_counts[first_bigram][third_token]
 
+        for third_token in trigram_counts[first_bigram]:
+            tri_gram = (first_bigram[0],first_bigram[1], third_token)
+            triTwitter_probabilities[tri_gram] = (trigram_counts[first_bigram][third_token] + 1) / (sum + vocab_size)
     return triTwitter_probabilities
 
 def generate_text_bigram(prob):
     current_word = "["
     generated_text = [current_word]
-    while current_word != "]":
-        if(len(generated_text)>50):
-            break
-        if (len(generated_text) > 4):
-            next_word = max(prob, key=lambda bi_gram: bi_gram[0] == current_word)[1]
-            generated_text.append(next_word)
-            current_word = next_word
-        else:
-            possible_next_words = [word for word in prob if word[0] == current_word and word[1] != "."]
-            probabilities = [prob[(word)] for word in possible_next_words]
-            next_word = random.choices(possible_next_words, probabilities)[0][1]
-            generated_text.append(next_word)
-            current_word = next_word
+    while current_word != "]" and len(generated_text) < 50:
+        '''next_word = max(prob, key=lambda bi_gram: bi_gram[0] == current_word)[1]
+        generated_text.append(next_word)
+        current_word = next_word
+        Eliminato metodo completamente deterministico a causa loop'''
+        possible_next_bigrams = [bigram for bigram in prob if bigram[0] == current_word]
+        probabilities = [prob[(bigram)] for bigram in possible_next_bigrams]
+        next_word = random.choices(possible_next_bigrams, probabilities)[0][1]
+        generated_text.append(next_word)
+        current_word = next_word
     return " ".join(generated_text[1:-1])
 
 def generate_text_trigram(prob):
     current_trigram = ("[", "[", "[")
     generated_text = list(current_trigram)
-    while current_trigram[-2:] != ("]", "]"):
-        if(len(generated_text)>100):
-            break
+    while current_trigram[-2:] != ("]", "]") and len(generated_text) < 50:
         if (len(generated_text) > 6):
             next_trigram = max(prob, key=lambda tri_gram: tri_gram[:2] == current_trigram[-2:])
             generated_text.append(next_trigram[-1])
@@ -122,10 +116,8 @@ if __name__ == "__main__":
     # Lettura CSV
     file_path = 'trump_twitter_archive/tweets.csv'
     df = read_csv(file_path)
-    #for sent in df:
-     #   print(sent)
 
-    bi_prob=learn_bi_gram_model_legit(df)
+    bi_prob=learn_bi_gram_model(df)
 
     tri_prob=learn_tri_gram_model(df)
     print("BIGRAM GENERATED SENTENCES")
